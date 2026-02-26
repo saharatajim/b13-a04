391bfbab958bdfdec50627319d82d6650e5d534d
saharatajim <sahara.tajim@gmail.com>
2026-02-26 17:47:08 +0600

    almost complete but still have to fix

--- script.js
+++ script.js
@@ -1,28 +1,132 @@
-// logic 1:all card section er total child er shongkhai hobe total job application er shongkha
 
-// eikhane total button ache 5ta
-// 1.All,2.interview,3.Rejected--->header section e
-// 4.INTERVIEW,5.REJECTED----->card
-
-
-// step-1 :1st ei all card section er child ke count kori.
+// all element declaration
+let mainSection=document.getElementById('mainSection')
+let showOnSection=document.getElementById('showOnSection')
 let allCardSection=document.getElementById('allCardSection')
 let totalApplication=allCardSection.children.length
-// step-2 :totalCount interviewCount rejectCount
-
-// by default total,interview,rejected 0 ache,ekhon dynamicaly function diye egulor counting plus-minus korte chacchi.
 let interviewJobCount=document.getElementById('interviewCount')
 let rejectedJobCount=document.getElementById('rejectCount')
 let againTotalCount=document.getElementById('againTotalCount')
-
 let totalCardForInterview=[]
 let totalCardForRejected=[]
 let currentStatus='All'
-
-// lets start with "Total".now catch Total er '0' element dhori
+let allToggleBtn=document.getElementById('allToggleBtn')
+let interviewToggleBtn=document.getElementById('interviewToggleBtn')
+let rejectToggleBtn=document.getElementById('rejectToggleBtn')
+let emptySection=document.getElementById('emptySection')
 let totalJobCount=document.getElementById('totalCount')
-function calculateTotalJobApllication(){
 
+// all event 
+mainSection.addEventListener('click',function(event)
+{
+// Interview
+if(event.target.classList.contains('cardInterviewBtn')){
+
+let parentNode=event.target.parentNode.parentNode
+let cardfirstTtext=parentNode.querySelector('.cardfirstTtext').innerText
+let cardSecondText=parentNode.querySelector('.cardSecondText').innerText
+let cardThirdText=parentNode.querySelector('.cardThirdText').innerText
+let cardFourthTtext=parentNode.querySelector('.cardFourthTtext').innerText
+
+// modifying the status btn
+let cardStatusBtn=parentNode.querySelector('.cardStatusBtn')
+cardStatusBtn.innerHTML=`
+<button class="border rounded-sm p-1 text-green-400  active:scale-95 cardInterviewBtn
+                                 " >INTERVIEW</button>
+`
+cardStatusBtn.style.backgroundColor="white";
+
+// now gathering all info to store it on an object
+let cardInfo={
+cardfirstTtext,
+cardSecondText,
+cardThirdText,
+cardFourthTtext,
+cardStatusBtn:'INTERVIEW'
+}
+
+let cardfirstTtextExist=totalCardForInterview.find(item=>item.cardfirstTtext==cardInfo.cardfirstTtext)
+
+if(!cardfirstTtextExist){
+totalCardForInterview.push(cardInfo)
+//  ShowInterviewSection()
+}
+// calculateTotalJobApllication()
+totalCardForRejected=totalCardForRejected.filter(item=>item.cardfirstTtext!=cardInfo.cardfirstTtext)
+
+if(currentStatus=="rejectToggleBtn"){
+    ShowRejectedSection()
+}
+calculateTotalJobApllication()
+
+}
+
+// reject
+else if(event.target.classList.contains('cardRejectedBtn')){
+
+let parentNode=event.target.parentNode.parentNode
+let cardfirstTtext=parentNode.querySelector('.cardfirstTtext').innerText
+let cardSecondText=parentNode.querySelector('.cardSecondText').innerText
+let cardThirdText=parentNode.querySelector('.cardThirdText').innerText
+let cardFourthTtext=parentNode.querySelector('.cardFourthTtext').innerText
+
+// modifying the status btn
+let cardStatusBtn=parentNode.querySelector('.cardStatusBtn')
+
+cardStatusBtn.innerHTML=`
+ <button class="border rounded-sm p-1 text-red-400 active:scale-95 cardRejectedBtn 
+                                ">REJECTED</button>
+`
+cardStatusBtn.style.backgroundColor="white";
+
+// now gathering all info to store it on an object
+let cardInfo={
+cardfirstTtext,
+cardSecondText,
+cardThirdText,
+cardFourthTtext,
+cardStatusBtn:'REJECTED'
+}
+
+let cardfirstTtextExist=totalCardForRejected.find(item=>item.cardfirstTtext==cardInfo.cardfirstTtext)
+
+if(!cardfirstTtextExist){
+totalCardForRejected.push(cardInfo)
+//  ShowInterviewSection()
+}
+// calculateTotalJobApllication()
+totalCardForInterview=totalCardForInterview.filter(item=>item.cardfirstTtext!=cardInfo.cardfirstTtext)
+
+if(currentStatus=="interviewToggleBtn"){
+    ShowInterviewSection()
+}
+calculateTotalJobApllication()
+
+}
+
+// delete
+else if(event.target.classList.contains('cardDeleteBtn')){
+let parentNode=event.target.parentNode.parentNode
+
+let cardfirstTtext=parentNode.querySelector('.cardfirstTtext').innerText
+
+parentNode.remove()
+
+
+totalCardForRejected=totalCardForRejected.filter(item=>item.cardfirstTtext!=cardfirstTtext)
+
+totalCardForInterview=totalCardForInterview.filter(item=>item.cardfirstTtext!=cardfirstTtext)
+
+totalApplication=allCardSection.children.length
+calculateTotalJobApllication()
+
+}
+})
+
+
+
+// calculation function
+function calculateTotalJobApllication(){
 totalJobCount.innerText=totalApplication
 againTotalCount.innerText=totalApplication
 interviewJobCount.innerText=totalCardForInterview.length
@@ -30,30 +134,9 @@ rejectedJobCount.innerText=totalCardForRejected.length
 
 }
 calculateTotalJobApllication()
-
-// now calc function complete
-
-// logic 2:job available section e 3ta button ache,
-// ami eder toggling korte chacchi,
-//  step-1 ; 1ta button e click korle shetai color change korbe,step -2:baki gulo other same color ei thakbe
-
-// ekhon ami button gulor id collect korechi,
-// 1.All=allToggleBtn,
-// 2.Interview=interviewToggleBtn,
-// 3.Rejected= rejectToggleBtn
-
-// step 3:ekta togglong function banabo jeno ta onclick ei kaaj kore,erjonno age button gulo ke dhore store kore rakhlam
-
-let allToggleBtn=document.getElementById('allToggleBtn')
-let interviewToggleBtn=document.getElementById('interviewToggleBtn')
-let rejectToggleBtn=document.getElementById('rejectToggleBtn')
-//  bg-white text-gray-500 --->thats for others
-//  bg-[#3B82F6] text-white --->when click any button that will turn this
-let emptySection=document.getElementById('emptySection')
-console.log(emptySection)
-
+// toggling function
  function togglingButton(id){
-// lets make all button unclicky color
+
     allToggleBtn.classList.remove('text-white','bg-[#3B82F6]')
     interviewToggleBtn.classList.remove('text-white','bg-[#3B82F6]')
     rejectToggleBtn.classList.remove('text-white','bg-[#3B82F6]')
@@ -61,36 +144,123 @@ console.log(emptySection)
     allToggleBtn.classList.add('bg-white','text-gray-500')
     interviewToggleBtn.classList.add('bg-white','text-gray-500')
     rejectToggleBtn.classList.add('bg-white','text-gray-500')
-
-currentStatus=id
+    
+    currentStatus=id
+    console.log(currentStatus)
 
      
     let selected=document.getElementById(id)
     selected.classList.remove('bg-white','text-gray-500')
     selected.classList.add('text-white','bg-[#3B82F6]')
 
-    if(currentStatus=='allToggleBtn'){
+    if(id=='allToggleBtn'){
     allCardSection.classList.remove('hidden')
     emptySection.classList.add('hidden')
+    showOnSection.classList.add('hidden')
     }
-    if(currentStatus=='interviewToggleBtn'){
-    allCardSection.classList.add('hidden')
-    emptySection.classList.remove('hidden')
+    else if(id=='interviewToggleBtn'){
+     allCardSection.classList. add('hidden')
+     emptySection.classList.remove('hidden')
+     ShowInterviewSection()
     }
-    if(currentStatus=='rejectToggleBtn'){
-   allCardSection.classList.add('hidden')
-    emptySection.classList.remove('hidden')
+    else if(id=='rejectToggleBtn'){
+     allCardSection.classList. add('hidden')
+     emptySection.classList.remove('hidden')
+    ShowRejectedSection()
     }
-//  togglig is almost done  
+
 
  }
 
+function ShowInterviewSection(){
+showOnSection.innerHTML=''
+for(let interviewCard of totalCardForInterview)
+    {
+let div=document.createElement('div')
+div.className='card  p-6 bg-white shadow rounded-sm mb-6 '
+div.innerHTML=
+`
+     <div class="flex justify-between items-center">
+                    <div>
+                        <p  class="cardfirstTtext text-[20px] font-semibold">${interviewCard.cardfirstTtext}</p>
+                        <p class="cardSecondText text-gray-400">${interviewCard.cardSecondText}</p>
+                     </div>
+                    
+                       <img class="active:scale-95 cardDeleteBtn" src="./reasource/Group 1.png" alt="">
+        </div>
 
-//  if i press
-// 1.INTERVIEW on the card-->pura card take object er vitore dhore store korbe
-// 2.REJECTED on the card-->
-// 3.delete icon button on the card-->
+       <div class="space-y-6">
+                        <p class="cardThirdText text-gray-400 ">${interviewCard.cardThirdText}</p>
+                        
+                       <button class="border rounded-sm p-1 text-green-400  active:scale-95 cardInterviewBtn
+                                 " >INTERVIEW</button>
 
-function renderInterview(){
-    
+                        <p class="cardFourthTtext">${interviewCard.cardFourthTtext}</p>
+        </div>
+
+    <div class="flex gap-2">
+                    <!-- clickable button section -->
+                    <button class="border rounded-sm p-1 text-green-400  active:scale-95 cardInterviewBtn
+                                 " >INTERVIEW</button>
+
+                    <button class="border rounded-sm p-1 text-red-400 active:scale-95 cardRejectedBtn 
+                                ">REJECTED</button>
+  </div>
+
+`
+showOnSection.appendChild(div)
+if(currentStatus=='interviewToggleBtn'){
+emptySection.classList.add('hidden')
+showOnSection.classList.remove('hidden')
 }
+
+}
+
+}
+function ShowRejectedSection(){
+showOnSection.innerHTML=''
+for(let rejectCard of totalCardForRejected)
+    {
+let div=document.createElement('div')
+div.className='card  p-6 bg-white shadow rounded-sm mb-6 '
+div.innerHTML=
+`
+     <div class="flex justify-between items-center">
+                    <div>
+                        <p  class="cardfirstTtext text-[20px] font-semibold">${rejectCard.cardfirstTtext}</p>
+                        <p class="cardSecondText text-gray-400">${rejectCard.cardSecondText}</p>
+                     </div>
+                    
+                       <img class="active:scale-95 cardDeleteBtn" src="./reasource/Group 1.png" alt="">
+        </div>
+
+       <div class="space-y-6">
+                        <p class="cardThirdText text-gray-400 ">${rejectCard.cardThirdText}</p>
+                    <button class="border rounded-sm p-1 text-red-400 active:scale-95 cardRejectedBtn 
+                                ">REJECTED</button>
+                        <p class="cardFourthTtext">${rejectCard.cardFourthTtext}</p>
+        </div>
+
+    <div class="flex gap-2">
+                    <!-- clickable button section -->
+                       <button class="border rounded-sm p-1 text-green-400  active:scale-95 cardInterviewBtn
+                                 " >INTERVIEW</button>
+
+                    <button class="border rounded-sm p-1 text-red-400 active:scale-95 cardRejectedBtn 
+                                ">REJECTED</button>
+  </div>
+
+`
+showOnSection.appendChild(div)
+
+if(currentStatus=='rejectToggleBtn'){
+emptySection.classList.add('hidden')
+showOnSection.classList.remove('hidden')
+}
+
+}
+// emptySection.classList.add('hidden')
+// showOnSection.classList.remove('hidden')
+
+}
+
